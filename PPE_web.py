26d90f301f54b1bcb884198ea61ba24243f4d3c5
Lilian Biscarrat <lilian.bscr@gmail.com>
2022-04-04 16:33:32 +0200

    add

--- PPE_web.py
+++ PPE_web.py
@@ -7,14 +7,21 @@ df=pd.read_csv(r'C:\Users\lilia\OneDrive\Documents\Lilian\ING4_S2\Untitled Folde
 d=database('C:/Users/lilia/OneDrive/Documents/Lilian/ING4_S2/Untitled Folder/Clean2gether.json')
 
 r=regression(df)
+mlr=pd.DataFrame(columns=["mlr_lat","mlr_long",'r2_lat','r2_long'])
 
-mlrlat,r2lat,rmselat,y_pred_mlrlat,y_trainlat=r.lat_reg()
-mlrlong,r2long,rmselong,y_pred_mlrlong,y_trainlong,x_test=r.long_reg()
+for i in range(1000):
+    mlrlat,r2lat,rmselat,y_pred_mlrlat,y_trainlat=r.lat_reg()
+    mlrlong,r2long,rmselong,y_pred_mlrlong,y_trainlong,x_test=r.long_reg()
+    mlr=mlr.append({"mlr_lat" :mlrlat ,"mlr_long" : mlrlong, 'r2_lat' :float(r2lat), 'r2_long' :float(r2long) } , ignore_index=True)
 
-print(r2lat)
-print(r2long)
-print(rmselat)
-print(rmselong)
+best_mlr_lat = mlr['mlr_lat'][mlr["r2_lat"].idxmax()]
+best_mlr_long = mlr['mlr_long'][mlr["r2_long"].idxmax()]
+best_r2lat = mlr['r2_lat'][mlr["r2_lat"].idxmax()]
+best_r2long = mlr['r2_long'][mlr["r2_long"].idxmax()]
+
+
+print(best_r2lat)
+print(best_r2long)
 
 """
 plot the total predictions thanks to the Clean2Gether dataset
